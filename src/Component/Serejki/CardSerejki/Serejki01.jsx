b25401bc7f82bf01bff2b0da9bd8c94032c24cd2
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-08-31 19:21:10 +0300

    gi

--- src/Component/Serejki/CardSerejki/Serejki01.jsx
+++ src/Component/Serejki/CardSerejki/Serejki01.jsx
@@ -13,7 +13,7 @@ function Serejki01(props) {
     return (
         <>
             <div className={c.content}>
-                <img className={c.imge} src="https://silverbeard.ru/wp-content/uploads/2020/12/Obruchalnye-koltsa-Vulkanicheskaya-lava.jpg" alt={'Photo'}/>
+                <img className={c.imge} src="https://anabel27.ru/wp-content/uploads/2024/02/00939-%D1%81%D0%B5%D1%80%D1%8C%D0%B3%D0%B8-%D0%B8%D0%B7-%D0%B1%D0%B8%D1%81%D0%B5%D1%80%D0%B0-4.jpg" alt={'Photo'}/>
                 <h2 className={c.naz}>{props.name}</h2>
                 <h2 className={c.price}>{props.price}p.</h2>
                 <button className={c.butt} onClick={handleClick}>Купить{kolca}</button>
